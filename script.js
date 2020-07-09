949c579cf58b3f209cc5bc223e0bd9411e3efc45
unknown <robertjamestoole@gmail.com>
2020-07-08 20:01:27 -0700

    First commit for my 2020 portfolio website

---
+++ script.js
@@ -0,0 +1,13 @@
+const scrollTopButton = document.getElementById("js-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopButton.style.display = "block";
+  } else {
+    scrollTopButton.style.display = "none";
+  }
+}
